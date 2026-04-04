e67d8643c3192f3e4d38a525f7b7ba8e6f292ab3
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-04-04 11:41:16 -0500

    Create model Driver and DriverLicense, using asosiation in both

---
+++ src/main/java/org/example/models/Driver.java
@@ -0,0 +1,71 @@
+package org.example.models;
+
+import java.time.LocalDate;
+
+public class Driver {
+    private int id_driver;
+    private int num_identification;
+    private String name;
+    private String lastName;
+    private String secondLastName;
+    private LocalDate contratationDate;
+    private DriverLicense license;
+
+    public Driver(int id_driver, int num_identification, String name, String lastName,
+                  String secondLastName, LocalDate contratationDate) {
+        this.id_driver = id_driver;
+        this.num_identification = num_identification;
+        this.name = name;
+        this.lastName = lastName;
+        this.secondLastName = secondLastName;
+        this.contratationDate = contratationDate;
+    }
+
+    // --- GETTERS ---
+
+    public int getId_driver() {
+        return id_driver;
+    }
+
+    public int getNum_identification() {
+        return num_identification;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSecondLastName() {
+        return secondLastName;
+    }
+
+    public LocalDate getContratationDate() {
+        return contratationDate;
+    }
+
+    public DriverLicense getLicense() {
+        return license;
+    }
+
+    // --- SETTERS ---
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSecondLastName(String secondLastName) {
+        this.secondLastName = secondLastName;
+    }
+
+    public void setLicense(DriverLicense license) {
+        this.license = license;
+    }
+}
