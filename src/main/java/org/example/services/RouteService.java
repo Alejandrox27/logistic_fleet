673427728778ef049bf3dbaaa030e6cc1ba57395
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-04-10 17:19:22 -0500

    Create the exception when the driver is fatigated

--- src/main/java/org/example/services/RouteService.java
+++ src/main/java/org/example/services/RouteService.java
@@ -1,5 +1,7 @@
 package org.example.services;
 
+import org.example.Exceptions.DriverExceptions.DriverException;
+import org.example.Exceptions.DriverExceptions.DriverFatigueException;
 import org.example.Exceptions.VehicleExceptions.VehicleException;
 import org.example.Exceptions.VehicleExceptions.VehicleNotAvailableException;
 import org.example.db.DriverDAO;
@@ -10,7 +12,7 @@ import org.example.models.VehicleStatus;
 import org.example.models.dto.DriverFatigueDTO;
 
 public class RouteService {
-    public String createRoute(Route route) throws VehicleException {
+    public String createRoute(Route route) throws VehicleException, DriverFatigueException {
         VehicleStatus currentStatus = VehicleDAO.checkDisponibility(route.getVehicle().getIdVehicle());
         DriverFatigueDTO driver = DriverDAO.getReportDriverFatigue(route.getDriver().getIdDriver());
 
@@ -23,7 +25,7 @@ public class RouteService {
         }
 
         if (driver != null) {
-
+            throw new DriverFatigueException(driver);
         }
 
         RoutesDAO.saveRoute(route);
