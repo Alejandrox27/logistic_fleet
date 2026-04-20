500934e3c6e9fce79fb68b222c726e8928652aca
Alejandrox27 <didieralejandromejia82@gmail.com>
2026-04-19 19:26:21 -0500

    Create the method to create a new driver in DriverService.java

--- src/main/java/org/example/services/DriverService.java
+++ src/main/java/org/example/services/DriverService.java
@@ -1,9 +1,13 @@
 package org.example.services;
 
+import org.example.db.DriverDAO;
 import org.example.models.Driver;
 
 public class DriverService implements IDriverService{
     @Override
     public void createDriver (Driver driver) {
+
+
+        DriverDAO.saveDriver(driver);
     }
 }
